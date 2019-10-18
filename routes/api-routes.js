b57b76bde3891961737b784f95054c04d36e3211
James Grodzicki <jamespgrodzicki@gmail.com>
2019-10-18 13:58:55 -0500

    hehe

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,8 +6,9 @@ module.exports = function(app) {
   app.post('/api/budget', function(req, res){
     db.Budget.create({
       category: req.body.category,
-      amount_spent: req.body.amount_spent
-    }).then(function(dbBudget){
+      amount_spent: req.body.amount_spent,
+      UserId: req.body.UserId
+    }).then(function(){
       res.redirect('/members');
     });
   });
