74b674c9e26c87b177f81fc099567d0a2f6a4300
Zaya <zayazaw67@gmail.com>
2019-10-18 12:01:13 -0500

    adjusted config.json - sent in slack to copy. moved userdata to budget + associated w/ user

--- models/budget.js
+++ models/budget.js
@@ -1,14 +1,21 @@
-module.exports = function(sequelize, DataTypes){
+module.exports = function (sequelize, DataTypes) {
     var Budget = sequelize.define('Budget', {
         category: {
             type: DataTypes.STRING,
             allowNull: false,
         },
         amount_spent: {
-            type: DataTypes.DECIMAL(10,2),
+            type: DataTypes.DECIMAL(10, 2),
             allowNull: false,
-            validate: {len: [1, 1000000]}
+            validate: { len: [1, 1000000] }
         }
     });
+    Budget.associate = function (models) {
+        Budget.belongsTo(models.Budget, {
+            foreignKey: {
+                allowNull: false
+            },
+        });
+    };
     return Budget;
 };
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
+
 var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
