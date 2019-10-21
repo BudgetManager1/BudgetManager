4e82a397b2e0d7976905fb1c4dffd7e861dabaff
Orlando Corona <corona.orlando@gmail.com>
2019-10-21 12:37:19 -0500

    Added modal form

--- public/js/members.js
+++ public/js/members.js
@@ -15,22 +15,28 @@ $(document).ready(function(){
   $('.collapsible').collapsible();
 });
 
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-  });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.dropdown-trigger');
+//     var instances = M.Dropdown.init(elems, options);
+    
+// });
 
   // Or with jQuery
 
-  $('.dropdown-trigger').dropdown();
-        
+  // $('.dropdown-trigger').dropdown();
+
+
+  // $('.dropdown-trigger + .dropdown-content').on('click', function(event) {
+  //   event.stopPropagation();
+  //   $('.dropdown-trigger').dropdown();
+  // });
+  
   document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
   });
-
-  // Or with jQuery
-
   $(document).ready(function(){
-    $('select').formSelect();
+    $('.modal').modal();
   });
+  
