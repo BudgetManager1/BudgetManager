d54d5017373cc878ed2c526f7b24b7a98d2c0ac4
Zaya <zayazaw67@gmail.com>
2019-10-21 11:25:05 -0500

    getting ready to merge

--- public/js/grabber.js
+++ public/js/grabber.js
@@ -8,12 +8,12 @@ $(document).ready(function () {
     $(document).on("submit", "#user-spending", grabUserSubmit);
 
     function grabUserSubmit(event) {
-        event.preventDefault();
-        $.get('/api/user_data', function (userData) {   //gets the current user data
+        event.preventDefault();                         
+        $.get('/api/user_data', function (userData) {   // gets the current user data
             // console.log(spentInput);
             var spentObj = spentInput.val().trim();     // console.log(spentTest);
-            var spentArr = spentObj.match(regEx);       //console.log(spentVal);  //spentVal returns as an object 
-            var spentVal = spentArr.join("");           //console.log(spentVal); converts back to string that only takes in integers and 2 decimal places
+            var spentArr = spentObj.match(regEx);       // console.log(spentVal);  // spentVal returns as an object 
+            var spentVal = spentArr.join("");           // console.log(spentVal); converts back to string that only takes in integers and 2 decimal places
             // console.log("clicked")
             // won't accept the form if fields have not been filled out
             if (!spentVal) {                            // console.log("user wants to submit form!") 
@@ -22,7 +22,7 @@ $(document).ready(function () {
             insertData({
                 amount_spent: spentVal,
                 category: category.val().trim(),
-                UserId: userData.id,                              // hard coded
+                UserId: userData.id,
             });
         });
     }
@@ -39,8 +39,9 @@ $(document).ready(function () {
             for (var i = 0; i < res.length; i++) {
                 rowsToAdd.push(createBudgetRow(res[i]));
             }
-            renderBudget(rowsToAdd);
+            // renderBudget(rowsToAdd);     // not created yet
             spentInput.val("");
+            category.val("");                       // remove once we create the dropdown menu for categories
         });
     }
 
--- routes/budgetdata-routes.js
+++ routes/budgetdata-routes.js
@@ -13,15 +13,15 @@ module.exports = function (app) {
     });
   });
 
-  app.post('/api/budget', function (req, res) {
-    db.Budget.create({
-      category: req.body.category,
-      amount_spent: req.body.amount_spent,
-      UserId: req.body.UserId
-    }).then(function () {
-      res.redirect('/members');
-    });
-  });
+  // app.post('/api/budget', function (req, res) {
+  //   db.Budget.create({
+  //     category: req.body.category,
+  //     amount_spent: req.body.amount_spent,
+  //     UserId: req.body.UserId
+  //   }).then(function () {
+  //     res.redirect('/members');
+  //   });
+  // });
 
 
   app.post("/api/budget", function (req, res) {
