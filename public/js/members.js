069bd2194bab030df3b1e9ace1539e9bcde1407d
Orlando Corona <corona.orlando@gmail.com>
2019-10-21 11:25:01 -0500

    Updated html w css

--- public/js/members.js
+++ public/js/members.js
@@ -14,3 +14,23 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function(){
   $('.collapsible').collapsible();
 });
+
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $('.dropdown-trigger').dropdown();
+        
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
