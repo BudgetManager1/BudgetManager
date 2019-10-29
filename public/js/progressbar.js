70f12fde57341bb9973fb014cff34ec9f26ef07d
James Grodzicki <jamespgrodzicki@gmail.com>
2019-10-29 04:01:55 -0500

    added user specific goal to page

--- public/js/progressbar.js
+++ public/js/progressbar.js
@@ -1,27 +1,21 @@
-var progressData = [0];
-var progress = ['goal'];
+var wish = $('#progress-bar').data('wish');
+var total = $('#progress-bar').data('total');
+var progress = $('#progress-bar').data('progress');
+var percentage = progress / total;
+console.log(wish + total + progress + percentage)
+Chart.defaults.global.defaultFontColor = '#000000'
+new Chart(document.getElementById("progress-bar").getContext('2d'), {
+    type: 'horizontalBar',
+    data: {
+        labels: ['% saved'],
+        datasets: [{
+            label: "Here's your progress",
+            backgroundColor: ["#3e95cd"],
+            data: [percentage],
+        }],
+    },
+    options: {
+        scales: { xAxes: [{ id: 'x-axis-1', type: 'linear', position: 'left', ticks: { min: 0, max: 1.00 } }] },
+    }
 
-$.get("/api/goals").then(function (data) {
-    // console.log(data);
-    Chart.defaults.global.defaultFontColor = '#000000'
-
-    new Chart(document.getElementById("progress-bar").getContext('2d'), {
-        type: 'horizontalBar',
-        data: {
-            labels: [`goal`],
-            datasets: [{
-                label: "Here's your progress",
-                backgroundColor: ["#3e95cd"],
-                data: [data[0].progress],
-            }],
-        },
-        options: {
-            scales: { xAxes: [{ id: 'x-axis-1', type: 'linear', position: 'left', ticks: { min: 0, max: 100 } }] },
-
-        }
-
-    });
-
-})
-
-
+});
